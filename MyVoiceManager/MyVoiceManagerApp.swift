7e6f0d2f7c3d995fc4b0e125925824ae51b8c185
Mingyu Hao <u6684258@anu.edu.au>
2022-11-22 01:05:26 +1100

    Initial Commit

---
+++ MyVoiceManager/MyVoiceManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyVoiceManagerApp.swift
+//  MyVoiceManager
+//
+//  Created by Mac on 2022/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MyVoiceManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
